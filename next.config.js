842e29c7fa6ebefe6876e71dec2a01f1a93e5f13
Troy Scott <troyscott_homeMac@Troys-MacBook-Pro-2.local>
2026-03-17 07:40:58 -0700

    Static export for GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  basePath: '/agm-mission-control',
+  images: {
+    unoptimized: true,
+  },
 }
 
 module.exports = nextConfig
